756b0e64c8c93f453fa0d4f32c29d6441d8438f3
juanpa0405 <66089289+juanpa0405@users.noreply.github.com>
2025-11-19 19:03:36 -0600

    Cambio 2
    Se agregaron ya las secciones del cine y clasesgrupales, se cambio el menu principal por un swing mas visual y con botones

--- SistemaRecepcion/src/Proyecto/Cine.java
+++ SistemaRecepcion/src/Proyecto/Cine.java
@@ -4,10 +4,26 @@
  */
 package Proyecto;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author juamp
  */
 public class Cine {
+
+    public String nombreCliente;
+    public int cantidadEntradas;
     
+    public void registrarReserva() {
+        nombreCliente = JOptionPane.showInputDialog("Nombre del cliente");
+        cantidadEntradas = Integer.parseInt(
+                JOptionPane.showInputDialog("Cantidad de entradas"));
+    }
+    
+    public void mostrarResumen() {
+        String msg = "Cliente: " + nombreCliente 
+                   + "\nEntradas: " + cantidadEntradas;
+        JOptionPane.showMessageDialog(null, msg);
+    }
 }
